ae8de2f62b6e94d17ce09dc88a47bd6b603c2d4e
Ben Venker <bvemails@gmai.com>
2020-07-20 15:15:53 -0500

    set up basic posts and users endpoints

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const port = process.env.PORT || 3001;
+const usersRouter = require('./users/usersRouter');
+const postsRouter = require('./posts/postsRouter');
 
 server.use(express.json());
 
@@ -10,4 +12,6 @@ server.get('/', (req, res) => {
   res.send('<h1>Welcome the Lambda Netork API!</h1>');
 });
 
+server.use('/users', usersRouter);
+server.use('/posts', postsRouter);
 module.exports = { server, port };
