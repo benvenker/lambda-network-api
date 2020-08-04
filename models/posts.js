67b5ddcfaba38e1c1495b444b81e5b454d1342db
Ben Venker <bvemails@gmai.com>
2020-08-04 13:07:06 -0500

    fix comments

--- models/posts.js
+++ models/posts.js
@@ -9,18 +9,7 @@ function get() {
   return db('posts');
 }
 
-/*
--- Get posts with comment, vote, and award counts
-select
-    *,
-    (select u.email from users u where u.id = posts.user_id),
-    (select count(id) as votes from votes v where v.post_id = posts.id),
-    (select count(id) as comments from comments c where c.post_id = posts.id),
-    (select count(id) as awards from post_awards a where a.post_id = posts.id)
-
-from posts where user_id = 'dbe86e29-34d6-436a-bb98-3e71fc65a24c'
-
-*/
+// Get posts with comment, vote, and award counts
 async function getPostsByUserId(userId) {
   const postsIdIdentifier = db.ref('id').withSchema('posts');
 
