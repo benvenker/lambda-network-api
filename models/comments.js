08fed15142410f8d50bf3b8a4034a3a7e86c049d
Jess <jessbonanno@gmail.com>
2020-08-04 17:10:20 -0400

    changed sql query to knex

--- models/comments.js
+++ models/comments.js
@@ -10,36 +10,46 @@ function get() {
   return db('comments');
 }
 
-// function getCommentByPostId(postId) {
-//   return db.raw(
-//     `
-//     select (
-//       select u.email
-//       from users u
-//       where u.id = c.user_id
-//   ),
-//   *,
-//   (
-//     select count(id) as comments
-//     from comments c
-//     where c.parent_comment_id = c.id
-// )
-// from comments c
-// where c.post_id = '${postId}'
-//     `
-//   );
-// }
-
+// * bens working solution
 function getCommentByPostId(postId) {
-  const comments = db('comments')
-    .join('users', 'user_id', 'comments.user_id')
+  const commentsIdIdentifier = db.ref('id').withSchema('comments');
+  const commentsUserIdIdentifier = db.ref('user_id').withSchema('comments');
+  const postIdentifier = db.ref('post_id').withSchema('comments');
+  const postUserIdIdentifier = db.ref('user_id').withSchema('posts');
+  const userIdentifier = db.ref('id').withSchema('users');
+  console.log(postId);
+  return db('comments')
+    .select(
+      function () {
+        this.select('posts.body as post')
+          .from('posts')
+          .where('posts.id', postIdentifier);
+      },
+      'posts.created_date as post created on',
+      'posts.title',
+      'posts.link',
+      function () {
+        this.select('email as posted by')
+          .from('users')
+          .where('users.id', postUserIdIdentifier);
+      },
+      function () {
+        this.select('email')
+          .from('users')
+          .where('users.id', commentsUserIdIdentifier)
+          .as('comment by');
+      },
+      'comments.body as comment',
+      'comments.created_date as comment created on',
+      // this is selecting the number of comments that the comment of the post has
+      function () {
+        this.count('id')
+          .as('comments on comment')
+          .from('comments')
+          .where('comments.parent_comment_id', commentsIdIdentifier);
+      }
+    )
+    .from('comments')
     .where('comments.post_id', postId)
-    .select('email', '*');
-  return comments;
+    .join('posts', 'posts.id', 'comments.post_id');
 }
-// * working code to return child comment count
-const childComments = db('comments')
-  .count('id as comments')
-  .where('comments.parent_comment_id', function () {
-    this.select('id').from('comments').where('comments.id', postId);
-  });
