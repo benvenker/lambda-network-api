8c05dcf367fa3e6ec6affc3d711bef9d55dbbca7
Ben Venker <bvemails@gmai.com>
2020-08-06 16:50:07 -0500

    get user by id

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,20 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err));
 });
 
-// router.get('/:id' (req, res) => {
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
 
-// })
+  return users
+    .getUserById(id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ message: 'No user found' });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch(err => res.status(500).json({ message: `Server error: ${err}` }));
+});
 
 router.get('/following/:followedId', (req, res) => {
   const { followedId } = req.params;
@@ -26,7 +37,7 @@ router.get('/following/:followedId', (req, res) => {
         res.status(200).json(followers);
       }
     })
-    .catch(err => console.log(err));
+    .catch(err => res.status(500).json({ message: `Server error: ${err}` }));
 });
 
 router.post('/', (req, res) => {
