551b1edf5a49bdcc9f7d6e4caab9f31a784fd99c
Ben Venker <bvemails@gmai.com>
2020-08-13 15:26:03 -0500

    get skills, add a skill, and get a skill by name endpoint

--- models/skills.js
+++ models/skills.js
@@ -2,8 +2,23 @@ const db = require('../data/dbConfig.js');
 
 module.exports = {
   get,
+  insert,
+  getSkillByName,
 };
 
 function get() {
   return db('skills');
+}
+
+function getSkillByName(name) {
+  const inputName = name.toLowerCase();
+  return db('skills').where({ name: inputName });
+}
+
+function insert(skill) {
+  try {
+    return db('skills').insert(skill, 'id');
+  } catch (err) {
+    console.log(err);
+  }
 }
--- routes/skills.js
+++ routes/skills.js
@@ -1,13 +1,59 @@
 const express = require('express');
 const skills = require('../models/skills.js');
+const UUID = require('uuid-1345');
+const { getSkillByName } = require('../models/skills.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return skills
-    .get()
-    .then(skills => res.status(200).json(skills))
-    .catch(err => res.status(500).json({ msg: err }));
+router.get('/', async (req, res) => {
+  try {
+    const skills = await skills.get();
+    return res.status(200).json(skills);
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
+});
+
+router.get('/by-name', async (req, res) => {
+  try {
+    const { name } = req.query;
+    if (!name) {
+      res.status(400).json({ message: 'Please include a name' });
+    } else {
+      const skill = await skills.getSkillByName(name);
+      return res.status(200).json(skill[0]); // Returns the skill name and id
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Name could not be retrieved' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ message: 'you must provide a skill' });
+  } else if (getSkillByName(name).length > 0) {
+    res.status(422).json({ message: 'The skill already exists' });
+  } else {
+    const skill = {
+      name: name.toLowerCase(),
+      id: UUID.v4(),
+    };
+    try {
+      const newSkill = await skills.insert(skill);
+      return res
+        .status(201)
+        .json({ message: 'new skill added', id: newSkill[0] });
+    } catch (err) {
+      if ((err.code = '23505')) {
+        res.status(400).json({ message: 'The skill already exists' });
+      } else {
+        res
+          .status(500)
+          .json({ message: 'There was an error adding the skill' });
+      }
+    }
+  }
 });
 
 module.exports = router;
