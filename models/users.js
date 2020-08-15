6399910c89966394c41bcc5808fda476be5bca61
Ben Venker <bvemails@gmai.com>
2020-08-15 15:54:15 -0500

    add user create user endopint

--- models/users.js
+++ models/users.js
@@ -2,12 +2,17 @@ const db = require('../data/dbConfig.js');
 
 module.exports = {
   get,
+  insert,
   getById,
   getAllUsersAUserIsFollowing,
 };
 
 function get() {
   return db('users');
+}
+
+function insert(user) {
+  return db('users').insert(user, 'id');
 }
 
 function getById(id) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const users = require('../models/users.js');
 const { validateUserId } = require('../middleware/usersMiddleware');
+const UUID = require('uuid-1345');
 
 const router = express.Router();
 
@@ -28,6 +29,26 @@ router.get('/:id/following', validateUserId(), (req, res) => {
       }
     })
     .catch(err => console.log(err));
+});
+
+// Create a new user
+router.post('/', async (req, res) => {
+  if (req.body) {
+    const user = {
+      id: UUID.v4(),
+      created_date: new Date(),
+      permission_type: 'user',
+      ...req.body,
+    };
+    try {
+      const newUser = await users.insert(user);
+      return res.status(200).json({ id: newUser[0] });
+    } catch (err) {
+      return res.status(500).json({ message: `Error creating user: ${err}` });
+    }
+  } else {
+    res.status(400).json({ message: 'No user body was provided' });
+  }
 });
 
 router.get('/');
